a2b1edb370e61da778e7b08cc41cf7ae8ec15209
dareiza <dareiza@gmail.com>
2020-02-29 12:05:26 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: Text('Pickup'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none),
+            onPressed: () {},
+          ),
+        ],
+      ),
+body: Container(
+          child: Column(
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Text('Categories',
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 15.0),
+          TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: Color(0xFFC88D67),
+              isScrollable: true,
+              labelPadding: EdgeInsets.only(right: 45.0),
+              unselectedLabelColor: Color(0xFFCDCDCD),
+              tabs: [
+                Tab(
+                  child: Text('Tab 1'),
+                ),
+                Tab(
+                  child: Text('Tab 2'),
+                ),
+                Tab(
+                  child: Text('Tab 3'),
+                ),
+              ]),
+              Expanded(
+    child: Container(
+        width: double.infinity,
+        child: TabBarView(controller: _tabController, children: [
+          CookiePage(),
+          CookiePage(),
+          CookiePage(),
+        ])),
+  )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(),
+floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+bottomNavigationBar: BottomBar(),
+    );
+  }
+}
